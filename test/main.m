7c091cd2e6ffc9b76e5b89d675c054fd7ff4b0ba
wangyingbo <wangyingbo528@126.com>
2017-08-28 12:58:55 +0800

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by 王迎博 on 2017/8/28.
+//  Copyright © 2017年 王颖博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
