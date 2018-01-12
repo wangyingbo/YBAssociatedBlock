4780623aeecf95a577644f76c8aabbf3453d06bd
wangyingbo <wangyingbo@edianzu.cn>
2018-01-12 15:43:29 +0800

    添加weakify和strongify

---
+++ test/YBTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  YBTestViewController.h
+//  test
+//
+//  Created by 王迎博 on 2018/1/12.
+//  Copyright © 2018年 王颖博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YBTestViewController : UIViewController
+
+@end
---
+++ test/YBTestViewController.m
@@ -0,0 +1,37 @@
+//
+//  YBTestViewController.m
+//  test
+//
+//  Created by 王迎博 on 2018/1/12.
+//  Copyright © 2018年 王颖博. All rights reserved.
+//
+
+#import "YBTestViewController.h"
+
+@interface YBTestViewController ()
+
+@end
+
+@implementation YBTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
